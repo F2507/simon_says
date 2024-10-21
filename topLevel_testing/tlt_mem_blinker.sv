cd8a8f3fb6df8fa9783def3dd177282c58ec4424
F2507 <70921957+F2507@users.noreply.github.com>
2024-10-21 10:33:42 -0400

    fixed address problem but needs more testing

--- topLevel_testing/tlt_mem_blinker.sv
+++ topLevel_testing/tlt_mem_blinker.sv
@@ -1,7 +1,9 @@
 module tlt_mem_blinker #( parameter hard_level = 4'd5,   parameter ms=1_000_000)
 (input logic CLOCK_50, input logic [3:0] KEY, output logic [9:0] LEDR, output logic [6:0] HEX0);
 
-reg [3:0] address = 4'bxxxx;
+reg [3:0] address;
+reg [3:0] count;
+
 wire [1:0] out_num;
 reg rw = 1;
 reg on_off = 0;
@@ -16,6 +18,7 @@ always @ (posedge CLOCK_50 && KEY[0] == 1 ) begin
         values <= i[1:0];
         i <= i+1;
     end else begin
+        address <= count;
         values <= 2'b00;
         rw <= 0;
         on_off <= 1;
@@ -35,7 +38,7 @@ blinker #(ms) blinker(.clk(CLOCK_50),
    .led_to_glow(out_num),  // led_to_glow is coming from simple_memory
 
     .done(done_W),              // done is going to the fsm to indicate that the module is done blinking 
-    .count(address),        // count is going to simple_memory as the address to read from
+    .count(count),        // count is going to simple_memory as the address to read from
     .led_out(LEDR)      // led_out is going to the top level module
 );
 
